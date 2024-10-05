c7a9308008c3a9e55d8e737576436cbae382d894
ochuks1 <cochuks@gmail.com>
2024-10-05 00:32:53 +0000

    Project task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,21 @@
+#!/usr/bin/python3
+"""
+Function that returns Pascal's triangle of size n.
+"""
+
 def pascal_triangle(n):
-    """ Returns a list of lists representing Pascal's triangle of n. """
+    """ Returns a list of lists representing Pascal's triangle of n """
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Initialize the first row of Pascal's triangle
+    triangle = [[1]]  # First row
 
     for i in range(1, n):
-        row = [1]  # Every row starts with 1
+        row = [1]
         prev_row = triangle[i - 1]
         for j in range(1, i):
-            # Each element is the sum of the two elements directly above it
             row.append(prev_row[j - 1] + prev_row[j])
-        row.append(1)  # Every row ends with 1
+        row.append(1)
         triangle.append(row)
 
     return triangle
